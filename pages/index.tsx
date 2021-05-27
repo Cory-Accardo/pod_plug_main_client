95838cdf2b7be8e2ef974ba250b122bbcec7fb37
Haowen Liu <liu.haowen.andy@gmail.com>
2021-05-27 11:04:52 -0700

    Add bg gradient, add Google Maps

--- pages/index.tsx
+++ pages/index.tsx
@@ -9,7 +9,37 @@ import Footer from "../components/Footer";
 import Header from "../components/Header";
 import Image from "../components/Image";
 
+import { GoogleMap, useJsApiLoader } from "@react-google-maps/api";
+import { useCallback, useState } from "react";
+
+const containerStyle = {
+  width: "100vw",
+  height: "52rem",
+};
+
+const mapOptions = {
+  mapTypeControl: false,
+  fullscreenControl: false,
+  streetViewControl: false,
+};
+
 export default function Home() {
+  const { isLoaded } = useJsApiLoader({
+    googleMapsApiKey: "AIzaSyCu5Vh4v5aQLJBSHVxzWeAOWHdy0_8pJaM",
+  });
+
+  const [map, setMap] = useState(null);
+
+  const onLoad = useCallback(function callback(map) {
+    const bounds = new window.google.maps.LatLngBounds();
+    map.fitBounds(bounds);
+    setMap(map);
+  }, []);
+
+  const onUnmount = useCallback(function callback(map) {
+    setMap(null);
+  }, []);
+
   return (
     <>
       <Head>
@@ -18,14 +48,14 @@ export default function Home() {
       </Head>
       {/* Header */}
       <Header current="/" />
-      <div className="text-lg text-center text-black shadow-md text-thin font-raleway">
+      <div className="mb-4 text-lg text-center text-black shadow-md text-thin font-raleway">
         Cashless Convenience in Age-gated Venues
       </div>
 
       {/* Main */}
       <main className="bg-background-blue">
         {/* First page */}
-        <div className="relative flex flex-row justify-around h-page">
+        <div className="relative flex flex-row justify-around h-page bg-gradient-to-b from-white to-background-blue">
           <div className="relative flex flex-col">
             <div
               className="mt-48 font-bold font-acumin text-7xl text-title-black"
@@ -70,11 +100,11 @@ export default function Home() {
         {/* Second page */}
         <div className="relative mt-32 h-page">
           <div
-            className="relative flex flex-col w-1/2 p-16 bg-white transform -translate-y-1/2 top-1/2 pr-60"
+            className="relative flex flex-col w-1/2 p-16 bg-white transform -translate-y-1/2 top-1/2 pr-60 font-raleway"
             style={{ borderRadius: "60px 0px 0px 60px", left: "15%" }}
           >
-            <div className="text-5xl">Convenience When Out</div>
-            <div className="mt-6 text-lg leading-loose">
+            <div className="text-5xl font-semibold">Convenience When Out</div>
+            <div className="mt-6 text-lg font-normal leading-loose">
               Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
               eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
               enim ad minim veniam, quis nostrud exercitation ullamco laboris
@@ -179,6 +209,17 @@ export default function Home() {
         </div>
         <div className="pt-64"></div>
       </main>
+      {isLoaded && (
+        <div className="w-screen h-page">
+          <GoogleMap
+            onLoad={onLoad}
+            onUnmount={onUnmount}
+            mapContainerStyle={containerStyle}
+            clickableIcons={false}
+            options={mapOptions}
+          ></GoogleMap>
+        </div>
+      )}
       <Footer />
     </>
   );
