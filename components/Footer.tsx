a9eb873e5a4198a6dc36fb0deaa19a7ccc06c3ed
Haowen Liu <liu.haowen.andy@gmail.com>
2021-05-30 08:17:31 -0700

    Add container layout

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -10,50 +10,57 @@ import { faBuilding, faPhone } from "@fortawesome/free-solid-svg-icons";
 
 export default function Footer() {
   return (
-    <div className="flex flex-row px-48 py-32">
-      <div className="flex flex-col mr-auto">
-        <div className="relative w-24">
-          <Image
-            src="/logo_dark_small"
-            alt="Pod Plug logo"
-            layout="responsive"
-            width={2097}
-            height={1294}
-          />
+    <div className="flex flex-col">
+      <div className="container">
+        <div className="flex flex-row py-32">
+          <div className="flex flex-col mr-auto">
+            <div className="relative w-24">
+              <Image
+                src="/logo_dark_small"
+                alt="Pod Plug logo"
+                layout="responsive"
+                width={2097}
+                height={1294}
+              />
+            </div>
+            <div className="mt-6 text-base font-roboto">
+              Copyright &copy; 2021 PodPlug
+            </div>
+            <div className="text-base font-roboto">
+              <Link href="/copyright">Privacy Policy</Link>
+            </div>
+            <div className="flex flex-row mt-4 gap-3 text-theme-dark">
+              <FontAwesomeIcon
+                icon={faInstagramSquare}
+                className="w-6 h-6"
+              ></FontAwesomeIcon>
+              <FontAwesomeIcon
+                icon={faFacebookSquare}
+                className="w-6 h-6"
+              ></FontAwesomeIcon>
+              <FontAwesomeIcon
+                icon={faBuilding}
+                className="w-6 h-6"
+              ></FontAwesomeIcon>
+              <FontAwesomeIcon
+                icon={faPhone}
+                className="w-6 h-6"
+              ></FontAwesomeIcon>
+            </div>
+          </div>
+          <div className="self-center mx-5 font-raleway">
+            <Link href="/">Home</Link>
+          </div>
+          <div className="self-center mx-5 font-raleway">
+            <Link href="/rewards">Rewards Venues</Link>
+          </div>
+          <div className="self-center mx-5 font-raleway">
+            <Link href="/about">About Us</Link>
+          </div>
+          <div className="self-center mx-5 font-raleway">
+            <Link href="/careers">Careers</Link>
+          </div>
         </div>
-        <div className="mt-6 text-base font-roboto">
-          Copyright &copy; 2021 PodPlug
-        </div>
-        <div className="text-base font-roboto">
-          <Link href="/copyright">Privacy Policy</Link>
-        </div>
-        <div className="flex flex-row mt-4 gap-3 text-theme-dark">
-          <FontAwesomeIcon
-            icon={faInstagramSquare}
-            className="w-6 h-6"
-          ></FontAwesomeIcon>
-          <FontAwesomeIcon
-            icon={faFacebookSquare}
-            className="w-6 h-6"
-          ></FontAwesomeIcon>
-          <FontAwesomeIcon
-            icon={faBuilding}
-            className="w-6 h-6"
-          ></FontAwesomeIcon>
-          <FontAwesomeIcon icon={faPhone} className="w-6 h-6"></FontAwesomeIcon>
-        </div>
-      </div>
-      <div className="self-center mx-5 font-raleway">
-        <Link href="/">Home</Link>
-      </div>
-      <div className="self-center mx-5 font-raleway">
-        <Link href="/rewards">Rewards Venues</Link>
-      </div>
-      <div className="self-center mx-5 font-raleway">
-        <Link href="/about">About Us</Link>
-      </div>
-      <div className="self-center mx-5 font-raleway">
-        <Link href="/careers">Careers</Link>
       </div>
     </div>
   );
--- pages/index.tsx
+++ pages/index.tsx
@@ -105,13 +105,11 @@ export default function Home() {
         return res.json();
       })
       .then((json) => {
-        json = json
-          .concat(json)
-          .concat(json)
-          .map((location: Location, index: number) => {
+        setLocations(
+          json.map((location: Location, index: number) => {
             return { ...location, index: index };
-          });
-        setLocations(json);
+          })
+        );
       });
   }, []);
 
@@ -164,30 +162,51 @@ export default function Home() {
         <Clouds id={2} />
         {/* First page */}
         <div
-          className="relative flex flex-row justify-around h-page"
+          className="flex flex-col h-page"
           style={{
             backgroundImage:
               "linear-gradient(to bottom, #FFF, #FFF 45%, #2D6EB7 100%)",
           }}
           ref={kioskContainer}
         >
-          <div className="relative flex flex-col z-content">
+          <div className="container relative flex flex-row justify-around ">
+            <div className="relative flex flex-col z-content">
+              <div
+                className="mt-48 font-bold font-acumin text-7xl text-title-black"
+                style={{ textShadow: "0px 4px 4px rgba(0, 0, 0, 0.25)" }}
+              >
+                Join Pod Rewards
+              </div>
+              <div className="mt-4 text-3xl font-semibold font-raleway text-subtitle-gray">
+                Get exclusive offers, discounts, and rebates
+              </div>
+              <input
+                type="text"
+                className="self-start w-3/4 p-1 mt-2 text-lg bg-white rounded-lg border-3 border-subtitle-gray text-subtitle-gray"
+              ></input>
+              <button className="self-start px-8 py-1 mt-3 text-lg font-semibold bg-white rounded-lg border-3 border-subtitle-gray text-subtitle-gray font-raleway">
+                Sign Up
+              </button>
+            </div>
             <div
-              className="mt-48 font-bold font-acumin text-7xl text-title-black"
-              style={{ textShadow: "0px 4px 4px rgba(0, 0, 0, 0.25)" }}
+              className="relative h-full z-content"
+              style={{ width: "calc(52rem / 2249 * 1315)" }}
             >
-              Join Pod Rewards
+              <div
+                className={`${kioskState === 0 && "relative"} ${
+                  kioskState === 1 && "fixed top-40px"
+                } ${kioskState === 2 && "relative top-272"}`}
+                style={{ width: "calc(52rem / 2249 * 1315)" }}
+              >
+                <Image
+                  src="/kiosk_full"
+                  alt="Pod Plug kiosk"
+                  layout="responsive"
+                  height={2249}
+                  width={1315}
+                />
+              </div>
             </div>
-            <div className="mt-4 text-3xl font-semibold font-raleway text-subtitle-gray">
-              Get exclusive offers, discounts, and rebates
-            </div>
-            <input
-              type="text"
-              className="self-start w-3/4 p-1 mt-2 text-lg bg-white rounded-lg border-3 border-subtitle-gray text-subtitle-gray"
-            ></input>
-            <button className="self-start px-8 py-1 mt-3 text-lg font-semibold bg-white rounded-lg border-3 border-subtitle-gray text-subtitle-gray font-raleway">
-              Sign Up
-            </button>
           </div>
           <div
             className="absolute bottom-0 block w-full z-bg"
@@ -198,39 +217,25 @@ export default function Home() {
               height: "24rem",
             }}
           ></div>
-          <div
-            className="relative h-full z-content"
-            style={{ width: "calc(52rem / 2249 * 1315)" }}
-          >
-            <div
-              className={`${kioskState === 0 && "relative"} ${
-                kioskState === 1 && "fixed top-40px"
-              } ${kioskState === 2 && "relative top-272"}`}
-              style={{ width: "calc(52rem / 2249 * 1315)" }}
-            >
-              <Image
-                src="/kiosk_full"
-                alt="Pod Plug kiosk"
-                layout="responsive"
-                height={2249}
-                width={1315}
-              />
-            </div>
-          </div>
         </div>
 
         {/* Second page */}
-        <div className="relative mt-gap h-page" ref={textContainer}>
-          <div
-            className="relative flex flex-col w-1/2 p-16 bg-white transform -translate-y-1/2 top-1/2 pr-60 font-raleway"
-            style={{ borderRadius: "60px 0px 0px 60px", left: "15%" }}
-          >
-            <div className="text-5xl font-semibold">Convenience When Out</div>
-            <div className="mt-6 text-lg font-normal leading-loose">
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-              eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
-              enim ad minim veniam, quis nostrud exercitation ullamco laboris
-              nisi ut aliquip ex ea commodo consequat.
+        <div
+          className="flex flex-col justify-center mt-gap h-page"
+          ref={textContainer}
+        >
+          <div className="container relative">
+            <div
+              className="relative flex flex-col p-16 mr-64 bg-white pr-60 font-raleway"
+              style={{ borderRadius: "60px 0px 0px 60px" }}
+            >
+              <div className="text-5xl font-semibold">Convenience When Out</div>
+              <div className="mt-6 text-lg font-normal leading-loose">
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+                eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
+                enim ad minim veniam, quis nostrud exercitation ullamco laboris
+                nisi ut aliquip ex ea commodo consequat.
+              </div>
             </div>
           </div>
         </div>
@@ -363,81 +368,85 @@ export default function Home() {
                 <Marker position={latlng} key={index} />
               ))}
             </GoogleMap>
-            <div className="absolute top-40 left-32 z-content">
-              <div className="text-4xl font-semibold font-raleway">
-                Our Kiosk Locations
-              </div>
-              <div className="flex flex-col px-8 pt-4 mt-8 bg-white rounded-xl max-h-96">
-                <div className="text-lg font-medium font-raleway">
-                  Your Location
-                </div>
-                <div className="flex flex-row mt-2">
-                  <form
-                    onSubmit={(e) => {
-                      e.preventDefault();
-                      submitSearch();
-                    }}
-                  >
-                    <input
-                      ref={searchBox}
-                      className="px-2 py-1 border-2 rounded-lg border-theme-light"
-                    ></input>
-                  </form>
-                  <button
-                    className="px-6 py-1 ml-4 text-white rounded-lg bg-theme-light"
-                    onClick={submitSearch}
-                  >
-                    Search
-                  </button>
-                </div>
-                <div className="mt-4 text-xs font-medium font-raleway">
-                  12 SEARCH RESULTS
-                </div>
-                <hr className="h-0 mx-3 mt-2 border-2 border-hr-gray"></hr>
-                <div className="flex-shrink my-3 overflow-y-scroll">
-                  <AnimateSharedLayout>
-                    <motion.div layout>
-                      {locations
-                        .filter(
-                          (location) =>
-                            currentSearch === "" ||
-                            location.name
-                              .toLowerCase()
-                              .includes(currentSearch) ||
-                            location.address.street
-                              .toLowerCase()
-                              .includes(currentSearch)
-                        )
-                        .map((location) => {
-                          return (
-                            <AnimatePresence key={location.index}>
-                              <motion.div
-                                className="flex flex-row items-center py-3"
-                                initial={{ opacity: 0 }}
-                                animate={{ opacity: 1 }}
-                                exit={{ opacity: 0 }}
-                                key={location.index}
-                                layout
-                              >
-                                <img
-                                  src="/marker.svg"
-                                  alt="Marker"
-                                  className="h-8"
-                                />
-                                <div className="flex flex-col ml-16">
-                                  <div className="text-base font-normal font-raleway">
-                                    {location.address.street}
-                                  </div>
-                                  <div className="text-sm font-medium font-raleway">
-                                    0.1 mi
-                                  </div>
-                                </div>
-                              </motion.div>
-                            </AnimatePresence>
-                          );
-                        })}
-                    </motion.div>
-                  </AnimateSharedLayout>
+            <div className="absolute top-0 left-0 flex flex-col w-full h-full">
+              <div className="container relative h-full">
+                <div className="absolute left-0 top-36 z-content">
+                  <div className="text-4xl font-semibold font-raleway">
+                    Our Kiosk Locations
+                  </div>
+                  <div className="flex flex-col px-8 pt-4 mt-8 bg-white rounded-xl max-h-96">
+                    <div className="text-lg font-medium font-raleway">
+                      Your Location
+                    </div>
+                    <div className="flex flex-row mt-2">
+                      <form
+                        onSubmit={(e) => {
+                          e.preventDefault();
+                          submitSearch();
+                        }}
+                      >
+                        <input
+                          ref={searchBox}
+                          className="px-2 py-1 border-2 rounded-lg border-theme-light"
+                        ></input>
+                      </form>
+                      <button
+                        className="px-6 py-1 ml-4 text-white rounded-lg bg-theme-light"
+                        onClick={submitSearch}
+                      >
+                        Search
+                      </button>
+                    </div>
+                    <div className="mt-4 text-xs font-medium font-raleway">
+                      12 SEARCH RESULTS
+                    </div>
+                    <hr className="h-0 mx-3 mt-2 border-2 border-hr-gray"></hr>
+                    <div className="flex-shrink my-3 overflow-y-scroll">
+                      <AnimateSharedLayout>
+                        <motion.div layout>
+                          {locations
+                            .filter(
+                              (location) =>
+                                currentSearch === "" ||
+                                location.name
+                                  .toLowerCase()
+                                  .includes(currentSearch) ||
+                                location.address.street
+                                  .toLowerCase()
+                                  .includes(currentSearch)
+                            )
+                            .map((location) => {
+                              return (
+                                <AnimatePresence key={location.index}>
+                                  <motion.div
+                                    className="flex flex-row items-center py-3"
+                                    initial={{ opacity: 0 }}
+                                    animate={{ opacity: 1 }}
+                                    exit={{ opacity: 0 }}
+                                    key={location.index}
+                                    layout
+                                  >
+                                    <img
+                                      src="/marker.svg"
+                                      alt="Marker"
+                                      className="h-8"
+                                    />
+                                    <div className="flex flex-col ml-16">
+                                      <div className="text-base font-normal font-raleway">
+                                        {location.address.street}
+                                      </div>
+                                      <div className="text-sm font-medium font-raleway">
+                                        0.1 mi
+                                      </div>
+                                    </div>
+                                  </motion.div>
+                                </AnimatePresence>
+                              );
+                            })}
+                        </motion.div>
+                      </AnimateSharedLayout>
+                    </div>
+                  </div>
                 </div>
               </div>
             </div>
