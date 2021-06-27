8d324b0dc8697cc90d0a048a1fd5429415575451
Haowen Liu <liu.haowen.andy@gmail.com>
2021-06-26 22:45:18 -0700

    Add brands

--- components/PartnersComponent.tsx
+++ components/PartnersComponent.tsx
@@ -5,14 +5,18 @@ export default function PartnersComponent() {
     <div className="flex flex-col items-center mt-24">
       <div className="w-auto pr-2 overflow-x-hidden md:overflow-y-scroll md:h-140 z-content py-2">
         <div className="grid grid-flow-row grid-cols-2 md:grid-cols-3 gap-3 md:gap-8 pb-4">
+          <BrandCard url="/juul.png" />
+          <BrandCard url="/vuse.png" />
           <BrandCard url="/airbar.png" />
-          <BrandCard url="/fume.png" />
-          <BrandCard url="/fume.png" />
-          <BrandCard url="/juul.png" />
-          <BrandCard url="/juul.png" />
-          <BrandCard url="/hqd.png" />
-          <BrandCard url="/hqd.png" />
-          <BrandCard url="/hqd.png" />
+          <BrandCard url="/velo.png" />
+          <BrandCard url="/zyn.png" />
+          <BrandCard url="/blu.png" />
+          <BrandCard url="/bidi.png" />
+          <BrandCard url="/morning_recovery.png" />
+          <BrandCard url="/drinkade.png" />
+          <BrandCard url="/icebreakers.png" />
+          <BrandCard url="/trident.png" />
+          <BrandCard url="/chargetab.png" />
         </div>
       </div>
     </div>
