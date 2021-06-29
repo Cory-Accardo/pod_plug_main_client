3dde1bd44f424361566bceb96314bb0f55406e4b
Haowen Liu <liu.haowen.andy@gmail.com>
2021-06-28 18:47:25 -0700

    Add clouds

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -44,18 +44,44 @@ function MyApp({ Component, pageProps }) {
       {!verified && (
         <>
           <div className="absolute h-full w-full bg-white opacity-50 z-overlay"></div>
-          <div className="fixed top-44 bg-white rounded-2xl w-96 h-136 z-overlay left-1/2 transform -translate-x-1/2 shadow-2xl flex flex-col items-center">
+          <div className="fixed top-44 bg-white rounded-2xl w-96 h-136 z-overlay left-1/2 transform -translate-x-1/2 shadow-2xl flex flex-col items-center overflow-hidden">
+            <div className="absolute w-full h-full">
+              <img
+                src="/clouds_gray.png"
+                alt="Clouds"
+                className="absolute transform scale-75"
+                style={{ left: "-10%" }}
+              ></img>
+              <img
+                src="/clouds_gray.png"
+                alt="Clouds"
+                className="absolute transform scale-75"
+                style={{
+                  left: "35%",
+                  top: "11rem",
+                }}
+              ></img>
+              <img
+                src="/clouds_gray.png"
+                alt="Clouds"
+                className="absolute"
+                style={{
+                  left: "30%",
+                  top: "26rem",
+                }}
+              ></img>
+            </div>
             <img
               src="/age.svg"
               alt="Age 21 warning"
-              className="w-48 mt-16 pt-2"
+              className="w-48 mt-16 pt-2 relative"
             />
-            <div className="font-black font-raleway text-2xl mt-4">
+            <div className="font-black font-raleway text-2xl mt-4 relative">
               Are you over 21?
             </div>
-            <div className="flex flex-row mt-20">
+            <div className="flex flex-row mt-20 relative">
               <button
-                className="font-raleway text-xl border-2 border-theme-dark rounded py-2 w-28 bg-button-emphasis hover:bg-button-hover"
+                className="font-raleway text-xl border-2 border-theme-dark rounded py-2 w-28 bg-button-emphasis hover:bg-button-hover relative"
                 onClick={() => {
                   verifyAge(true);
                 }}
@@ -63,7 +89,7 @@ function MyApp({ Component, pageProps }) {
                 Yes!
               </button>
               <button
-                className="font-raleway text-xl border-2 border-theme-dark rounded py-2 w-28 ml-10 hover:bg-button-hover"
+                className="font-raleway text-xl border-2 border-theme-dark rounded py-2 w-28 ml-10 bg-white hover:bg-button-hover relative"
                 onClick={() => {
                   verifyAge(false);
                 }}
