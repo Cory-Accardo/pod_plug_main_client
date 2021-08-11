9ed5157a2c1f9e048dad2b0dd029b1c224749795
Haowen Liu <liuhw@mail.com>
2021-08-11 12:29:27 -0700

    Add veridas

--- constants.ts
+++ constants.ts
@@ -2,8 +2,10 @@ export const GOOGLE_API_KEY = "AIzaSyCu5Vh4v5aQLJBSHVxzWeAOWHdy0_8pJaM";
 
 export const USER_ONLY = "https://api.podplug.com/main/user_only";
 
-export const API = "https://api.podplug.com"
+export const API = "https://api.podplug.com";
 
 export const JSON_HEADER = {
   "Content-Type": "application/json",
 };
+
+export const VERIDAS_URL = "xpressid-work.eu.veri-das.com";
---
+++ veridasOptions.ts
@@ -0,0 +1,202 @@
+export const dlOptions = {
+  Alaska: {
+    "2005": "US-AK_DrivingLicense_2005",
+    "2014": "US-AK_DrivingLicense_2014",
+    "2018": "US-AK_DrivingLicense_2018",
+  },
+  Alabama: {
+    "2013": "US-AL_DrivingLicense_2013",
+  },
+  Arkansas: {
+    "2016": "US-AR_DrivingLicense_2016",
+    "2018": "US-AR_DrivingLicense_2018",
+  },
+  Arizona: {
+    "1990": "US-AZ_DrivingLicense_1990",
+    "1996": "US-AZ_DrivingLicense_1996",
+    "2004": "US-AZ_DrivingLicense_2004",
+    "2016": "US-AZ_DrivingLicense_2016",
+  },
+  California: {
+    "2008": "US-CA_DrivingLicense_2008",
+    "2018": "US-CA_DrivingLicense_2018",
+  },
+  Colorado: {
+    "2011": "US-CO_DrivingLicense_2011",
+    "2016": "US-CO_DrivingLicense_2016",
+  },
+  Connecticut: {
+    "2017": "US-CT_DrivingLicense_2017",
+  },
+  "Washington D.C.": {
+    "2017": "US-DC_DrivingLicense_2017",
+  },
+  Delaware: {
+    "2010": "US-DE_DrivingLicense_2010",
+    "2018": "US-DE_DrivingLicense_2018",
+  },
+  Florida: {
+    "2010": "US-FL_DrivingLicense_2010",
+    "2017": "US-FL_DrivingLicense_2017",
+  },
+  Georgia: {
+    "2007": "US-GA_DrivingLicense_2007",
+    "2012": "US-GA_DrivingLicense_2012",
+  },
+  Hawaii: {
+    "2012": "US-HI_DrivingLicense_2012",
+    "2018": "US-HI_DrivingLicense_2018",
+  },
+  Iowa: {
+    "2013": "US-IA_DrivingLicense_2013",
+    "2018": "US-IA_DrivingLicense_2018",
+  },
+  Idaho: {
+    "2004": "US-ID_DrivingLicense_2004",
+    "2010": "US-ID_DrivingLicense_2010",
+    "2017": "US-ID_DrivingLicense_2017",
+  },
+  Illinois: {
+    "2007": "US-IL_DrivingLicense_2007",
+    "2016": "US-IL_DrivingLicense_2016",
+  },
+  Indiana: {
+    "2010": "US-IN_DrivingLicense_2010",
+    "2017": "US-IN_DrivingLicense_2017",
+  },
+  Kansas: {
+    "2004": "US-KS_DrivingLicense_2004",
+    "2012": "US-KS_DrivingLicense_2012",
+    "2017": "US-KS_DrivingLicense_2017",
+  },
+  Kentucky: {
+    "2012": "US-KY_DrivingLicense_2012",
+    "2019": "US-KY_DrivingLicense_2019",
+  },
+  Louisiana: {
+    "2011": "US-LA_DrivingLicense_2011",
+    "2014": "US-LA_DrivingLicense_2014",
+    "2016": "US-LA_DrivingLicense_2016",
+  },
+  Massachusetts: {
+    "2010": "US-MA_DrivingLicense_2010",
+    "2018": "US-MA_DrivingLicense_2018",
+  },
+  Maryland: {
+    "2013": "US-MD_DrivingLicense_2013",
+    "2016": "US-MD_DrivingLicense_2016",
+  },
+  Maine: {
+    "2011": "US-ME_DrivingLicense_2011",
+  },
+  Michigan: {
+    "2017": "US-MI_DrivingLicense_2017",
+  },
+  Minnesota: {
+    "2004": "US-MN_DrivingLicense_2004",
+    "2018": "US-MN_DrivingLicense_2018",
+  },
+  Missouri: {
+    "2004": "US-MO_DrivingLicense_2004",
+    "2012": "US-MO_DrivingLicense_2012",
+  },
+  Mississippi: {
+    "2001": "US-MS_DrivingLicense_2001",
+    "2017": "US-MS_DrivingLicense_2017",
+  },
+  Montana: {
+    "2000": "US-MT_DrivingLicense_2000",
+    "2008": "US-MT_DrivingLicense_2008",
+  },
+  "North Carolina": {
+    "2007": "US-NC_DrivingLicense_2007",
+    "2017": "US-NC_DrivingLicense_2017",
+  },
+  "North Dakota": {
+    "2006": "US-ND_DrivingLicense_2006",
+    "2018": "US-ND_DrivingLicense_2018",
+  },
+  Nebraska: {
+    "2013": "US-NE_DrivingLicense_2013",
+  },
+  "New Hampshire": {
+    "2006": "US-NH_DrivingLicense_2006",
+    "2017": "US-NH_DrivingLicense_2017",
+  },
+  "New Jersey": {
+    "2011": "US-NJ_DrivingLicense_2011",
+  },
+  "New Mexico": {
+    "2014": "US-NM_DrivingLicense_2014",
+    "2016": "US-NM_DrivingLicense_2016",
+  },
+  Nevada: {
+    "2014": "US-NV_DrivingLicense_2014",
+  },
+  "New York": {
+    "2008": "US-NY_DrivingLicense_2008",
+    "2017": "US-NY_DrivingLicense_2017",
+  },
+  Ohio: {
+    "2013": "US-OH_DrivingLicense_2013",
+    "2014": "US-OH_DrivingLicense_2014",
+    "2018": "US-OH_DrivingLicense_2018",
+  },
+  Oklahoma: {
+    "2018": "US-OK_DrivingLicense_2018",
+  },
+  Oregon: {
+    "2007": "US-OR_DrivingLicense_2007",
+    "2018": "US-OR_DrivingLicense_2018",
+  },
+  Pennsylvania: {
+    "2011": "US-PA_DrivingLicense_2011",
+    "2017": "US-PA_DrivingLicense_2017",
+  },
+  "Rhode Island": {
+    "2008": "US-RI_DrivingLicense_2008",
+    "2018": "US-RI_DrivingLicense_2018",
+  },
+  "South Carolina": {
+    "2011": "US-SC_DrivingLicense_2011",
+    "2018": "US-SC_DrivingLicense_2018",
+  },
+  "South Dakota": {
+    "2010": "US-SD_DrivingLicense_2010",
+  },
+  Tennessee: {
+    "2003": "US-TN_DrivingLicense_2003",
+    "2012": "US-TN_DrivingLicense_2012",
+  },
+  Texas: {
+    "2016": "US-TX_DrivingLicense_2016",
+  },
+  Utah: {
+    "2006": "US-UT_DrivingLicense_2006",
+    "2016": "US-UT_DrivingLicense_2016",
+  },
+  Virginia: {
+    "2018": "US-VA_DrivingLicense_2018",
+  },
+  Vermont: {
+    "2014": "US-VT_DrivingLicense_2014",
+    "2018": "US-VT_DrivingLicense_2018",
+  },
+  Washington: {
+    "2010": "US-WA_DrivingLicense_2010",
+    "2017": "US-WA_DrivingLicense_2017",
+    "2019": "US-WA_DrivingLicense_2019",
+  },
+  Wisconsin: {
+    "2005": "US-WI_DrivingLicense_2005",
+    "2012": "US-WI_DrivingLicense_2012",
+    "2015": "US-WI_DrivingLicense_2015",
+  },
+  "West Virginia": {
+    "2005": "US-WV_DrivingLicense_2005",
+    "2013": "US-WV_DrivingLicense_2013",
+  },
+  Wyoming: {
+    "2014": "US-WY_DrivingLicense_2014",
+  },
+};
